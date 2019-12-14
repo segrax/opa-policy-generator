f34ba4d8786a827afa57944b943e93ea976ccdb4
Robert Crossfield <robcrossfield@gmail.com>
2019-12-14 18:49:17 +1100

    Add cli app

---
+++ src/main.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Console;
+use PDO;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
